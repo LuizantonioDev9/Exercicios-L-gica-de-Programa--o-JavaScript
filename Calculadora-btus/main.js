3725ccbe737e0d4c934f0afa901a193ec397c8af
Luiz Antonio <luizantoniodev9@gmail.com>
2023-11-14 09:24:17 -0300

    calculadora-btu

---
+++ Calculadora-btus/main.js
@@ -0,0 +1,25 @@
+function calcular() {
+    let metrosQuadrados = document.getElementById('numMetros').value;
+    let numeroDePessoas = document.getElementById('numPessoas').value;
+    let eletronicos = document.getElementById('numEletronicos').value;
+    let saida = document.getElementById('saida');
+    let btu = 600;
+    saida.style.fontSize = '1.2em'
+    saida.style.fontFamily =  'Gill Sans, Gill Sans MT, Calibri, Trebuchet MS, sans-serif';
+
+    numeroDePessoas = numeroDePessoas * btu;
+    metrosQuadrados = metrosQuadrados * btu;
+    eletronicos = eletronicos * btu;
+    let totalBtu = numeroDePessoas + metrosQuadrados + eletronicos;
+
+    if(totalBtu <= 15000 && totalBtu >= 12000) {
+        totalBtu = 12000;
+        saida.innerHTML += `<p>${totalBtu} BTU</p>`;
+        console.log(totalBtu);
+    } else {
+        saida.innerHTML += `<p>${totalBtu} BTU</p>`
+    }
+
+    
+
+}
